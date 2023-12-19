ac5e01ec593d29a5bb58ffc6fd77cc9be45257f6
aomsumita <sumita_n@ac.th.>
2023-12-19 11:32:56 +0700

    lab9_2.cpp completed

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,7 +1,18 @@
 #include<iostream>
 using namespace std;
 
-//Write the function printO() here
+void printO(int x,int y){
+if (x <= 0 || y <= 0){
+cout << "Invalid input" ;
+}else{
+	for(int i = 0; i < x; i++){
+		for (int Z = 0; Z < y; Z++){
+			cout << 'O' ;
+			}
+			cout << '\n' ;
+	}
+	}
+}
 
 int main(){
 	
